54b32d32f97ae432bfef73e785be9b3752b6e92c
seungryeol <opop0421@naver.com>
2022-08-19 01:16:18 +0900

    jwt토큰 인코딩

--- routes/music_search.js
+++ routes/music_search.js
@@ -3,44 +3,64 @@ const http = require('http');
 const Music = require('../models/music');
 const router = express.Router();
 const sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
 
+const { verifytoken } = require("./middlewares");
 
-router.get('/', async (req, res, next) => {
+router.get('/', verifytoken, async (req, res, next) => {
   try {
-    const authCompWords = await Music.findAll({
-       attributes:['title','img','backgroung_url']
+    const favorite_user = await Music.findAll({         //favorite 테이블에서 인코드한 user데이터 가져오기
+      attributes:['id'],
+      where: { user_id :  req.decoded  },
+     });
+    const background_sounds = await Music.findAll({           
+       attributes:['id','name','sound_play_time','sound_img_url']
       });
-      const returnData =authCompWords.map((el) => el);
 
+
+      const returnData =background_sounds.map((el) => el);
       console.log(returnData);
-      return res.send(returnData);
+      return res.status(200).json(returnData,
+        {
+        code:200
+      }
+        );
     
   } catch (error) {
     console.error(error);  
+    res.status(500);
   }
 });
+
+
 
 router.get('/:background-sound-name/search', async (req, res, next) => {
   const Op = sequelize.Op;
   const get_title = req.params.background-sound-name;
 
-  get_title = get_title.replace(" ", "%"); 
+  get_title0 = get_title.replace(" ", "%"); 
           //검색의 띄어쓰기 해결 맨 앞 제목 한번만 가능
   try {
     const authCompWords = await Music.findAll({
-        where: { title: { [Op.like]: '%' + get_title + '%' } },
+      attributes:['id','name','sound_play_time','sound_img_url'],
+        where: { title : { [Op.like]: '%' + get_title0 + '%' } },
       });
       const returnData =authCompWords.map((el) => el);
       if(returnData == ""){
-        return res.send("404");  //해당 값이 없다는거임
+        return res.status("404");  //해당 값이 없다는거임
       }
       console.log(returnData);
-      return res.send(returnData);
+      return res.status(200).json(returnData);
     
   } catch (error) {
     console.error(error);  
+    res.status(500);
   }
 });
+
+
+
+
 
 router.get('/:background-sound-id/play', async (req, res, next) => {
   const Op = sequelize.Op;
@@ -48,16 +68,17 @@ router.get('/:background-sound-id/play', async (req, res, next) => {
  
   try {
     const authCompWords = await Music.findAll({
-       attributes:['title','img','backgroung_url'],
-       where: { title:  get_title  },
+       attributes:['id','name','sound_play_time','sound_img_url'],
+       where: { name:  get_title  },
       });
       const returnData =authCompWords.map((el) => el);
 
       console.log(returnData);
-      return res.send(returnData);
+      return res.status(200).json(returnData);
   }
  catch (error) {
     console.error(error);  
+    res.status(500);
   }
 });
 
