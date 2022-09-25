c1991c2fb4b50574f5f79afa13b0f2b04316e4b4
seungryeol <opop0421@naver.com>
2022-09-25 17:07:13 +0900

    소리 조회 페이지 유닛테스트(플레이,즐겨찾기 추가) 완료

--- routes/music_search_page.test.js
+++ routes/music_search_page.test.js
@@ -13,7 +13,7 @@ describe("Music_search API",  ()=> {
         
         request(app)
             .get("/background-sounds")
-            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDM2NDU4LCJleHAiOjE2NjQwNzI0NTgsImlzcyI6IkxlZSJ9.C52SD57JK_5hcX-TZWKeV-qsX-C9HOvHNgcHYfXvsqo')
+            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDkwNDI5LCJleHAiOjE2NjQxMjY0MjksImlzcyI6IkxlZSJ9.UqdvqCmEYqmCVUmWggw301t0EveCQK4ZKcGvEai1KxE')
             .expect(200)
             .end(done);
             
@@ -23,11 +23,35 @@ describe("Music_search API",  ()=> {
         const background_sound_name = "해변"
        request(app)
             .get(`/background-sounds/${encodeURI(background_sound_name)}/search`)
-            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDM2NDU4LCJleHAiOjE2NjQwNzI0NTgsImlzcyI6IkxlZSJ9.C52SD57JK_5hcX-TZWKeV-qsX-C9HOvHNgcHYfXvsqo')    
+            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDkwNDI5LCJleHAiOjE2NjQxMjY0MjksImlzcyI6IkxlZSJ9.UqdvqCmEYqmCVUmWggw301t0EveCQK4ZKcGvEai1KxE')    
+            .expect(200)
+            .end(done);      
+      });
+
+      it("GET  play music",  (done) => {
+        const background_sound_id = 3
+       request(app)
+            .get(`/background-sounds/${background_sound_id}/play`)
+            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDkwNDI5LCJleHAiOjE2NjQxMjY0MjksImlzcyI6IkxlZSJ9.UqdvqCmEYqmCVUmWggw301t0EveCQK4ZKcGvEai1KxE')    
             .expect(200)
             .end(done);
                 
       });
+
+      it("POST  favorite music",  (done) => {
+        const background_sound_id = 3
+       request(app)
+            .post(`/background-sounds/${background_sound_id}/favorite`)
+            .set('ACCESS_TOKEN', 'Beerer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjAsInB3ZCI6ImNuZXd3IiwiaWF0IjoxNjY0MDkwNDI5LCJleHAiOjE2NjQxMjY0MjksImlzcyI6IkxlZSJ9.UqdvqCmEYqmCVUmWggw301t0EveCQK4ZKcGvEai1KxE')    
+            .send({is_favorite:'false'})
+            .expect(200)
+            .end(done);
+                
+      });
+
+
+
+
 
 
 
