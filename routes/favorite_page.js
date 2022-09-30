a44638fb4987305c6679d72b9c76fc7cb6f26d48
CokeLee777 <akak640@kyonggi.ac.kr>
2022-09-30 15:15:59 +0900

    Refactoring code and add ci/cd code

--- routes/favorite_page.js
+++ routes/favorite_page.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const http = require('http');
 const  models  = require('../models/index');
 const sequelize = require('sequelize');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
 const { verifyToken } = require("./middlewares");
 const { Client } = require('pg');
-const { equal } = require('assert');
 require('dotenv').config();
 const env = process.env;
 const client = new Client({
@@ -14,7 +11,7 @@ const client = new Client({
     host: env.DATABASE_HOST,
     database: env.DATABASE_NAME,
     password: env.DATABASE_PASSWORD,
-    port: env.PORT
+    port: env.DATABASE_PORT
 });
 client.connect();
 
