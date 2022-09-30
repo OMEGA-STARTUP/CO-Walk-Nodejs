4d318935d743248fd202e86e24e575b1965ceff1
CokeLee777 <akak640@kyonggi.ac.kr>
2022-09-30 16:07:08 +0900

    Revise render code

--- routes/health_check.js
+++ routes/health_check.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.sendStatus(200).send('Health status: UP');
+    res.status(200).send('Health status: UP');
 });
 
 module.exports = router;
